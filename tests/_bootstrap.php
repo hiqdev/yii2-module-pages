91b6a26193932a982fed11691b529e5e73fc8eda
Andrii Vasyliev <sol@hiqdev.com>
2017-10-05 08:28:41 +0000

    inited empty tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Yii2 Pages Module
+ *
+ * @link      https://github.com/hiqdev/yii2-module-pages
+ * @package   yii2-module-pages
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+$bootstrap = __DIR__ . '/../src/_bootstrap.php';
+
+require_once file_exists($bootstrap) ? $bootstrap : __DIR__ . '/../vendor/autoload.php';
+
+/*
+ * Ensures compatibility with PHPUnit 6.x
+ */
+if (!class_exists('PHPUnit_Framework_Constraint') && class_exists('PHPUnit\Framework\Constraint\Constraint')) {
+    abstract class PHPUnit_Framework_Constraint extends \PHPUnit\Framework\Constraint\Constraint
+    {
+    }
+}
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    abstract class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
+    {
+    }
+}
